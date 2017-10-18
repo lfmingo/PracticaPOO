1497763b37a123d6ec8087d63a18cd8bfb90e43a
lfmingo <lfmingo@gmail.com>
2017-10-18 22:44:32 +0200

    dd

---
+++ src/test/java/etsisi/EquipoTest.java
@@ -0,0 +1,14 @@
+package etsisi;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EquipoTest {
+    @Test
+    public void todosPartidos() throws Exception {
+        assertEquals(3,2);
+    }
+
+
+}
