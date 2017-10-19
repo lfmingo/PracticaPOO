04f85fe8cbd1efc063b4cf82691a103d920a1ddd
lfmingo <lfmingo@gmail.com>
2017-10-19 11:54:45 +0200

    d

--- src/test/java/etsisi/EquipoTest.java
+++ src/test/java/etsisi/EquipoTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.*;
 public class EquipoTest {
     @Test
     public void todosPartidos() throws Exception {
+        Equipo e = new Equipo("R. Madrid");
         assertEquals(2,2);
     }
 
